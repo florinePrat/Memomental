95501b2e8d0f1395fa402a0914aa32018cef42ad
Florine <florineptlg64@gmail.com>
2019-05-11 14:12:02 +0100

    creation menu

---
+++ memomental/src/components/headerComponent/menu.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import logo from './images/logo.jpg';
+import RBN from 'react-burger-nav';
+import {
+    Link
+} from 'react-router-dom';
+
+class Header extends Component{
+
+    state = {
+        active: false,
+    };
+
+    render(){
+        return(
+            <header>
+                <div className="align">
+                    <div className="logo">
+                        <img src={logo} alt="logo"/>
+                    </div>
+                    <div className="titre">
+                        <h2> Memomental </h2>
+                    </div>
+                </div>
+
+                <RBN>
+                    <ul>
+                        <li className="first">
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/Signup">Inscription</Link>
+                        </li>
+                        <li className="last">
+                            <Link to="/Login">Connexion</Link>
+                        </li>
+                    </ul>
+                </RBN>
+
+
+
+
+            </header>
+        )
+    }
+}
+
+
+
+export default Header;
