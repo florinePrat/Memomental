9e53ad39db4287281ef42c991f1556a58f37a540
Florine <florineptlg64@gmail.com>
2019-05-16 19:02:10 +0100

    # - port heroku

--- memomental/src/utils/auth.js
+++ memomental/src/utils/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {basicHeaders} from './headers';
 
-const burl = "https://memomental.herokuapp.com";
+const burl = "https://memomental.herokuapp.com:" + process.env.PORT;
 
 // this is the request for authentification
 export default {
