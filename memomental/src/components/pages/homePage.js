a9f4fd5dc4a2c980fc6cbe5d6da0c1934ce785e6
Florine <florineptlg64@gmail.com>
2019-05-18 23:15:16 +0100

    #front - new home page design

--- memomental/src/components/pages/homePage.js
+++ memomental/src/components/pages/homePage.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 import {Button} from "react-bootstrap";
+import giphy from "./images/giphy.gif";
 
 // this page is the home page before the signup
 class homePage extends Component{
     render(){
         return(
-                    <div className="boxcarte">
+                    <div className="homePage">
 
                         <p> Est-tu prêt à booster ta mémoire ??</p>
-
-
+                        <img
+                            className="d-block w-100"
+                            src={giphy}
+                            alt="First slide"
+                        />
                         <Link to="/signup">
                             <Button
                                 className="btn-info"
